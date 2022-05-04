a161cb01087d85a848c17d7bd7c05458c76b5238
cryptondee <cryptondee@gmail.com>
2022-05-04 11:17:55 +0200

    finished section 9 chapter 107

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -33,39 +33,45 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-// 1) destructering
-// spread because on right side of =
-const arr = [1, 2, ...[3, 4]];
-//rest because on left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+console.log(0 || 'jonas');
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mush', 'spinahc');
+}
+restaurant.orderPizza && restaurant.orderPizza('mush', 'spinach');
 
-const [pizza, , risotto, ...otherfoods] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherfoods);
-// objects
+// // 1) destructering
+// // spread because on right side of =
+// const arr = [1, 2, ...[3, 4]];
+// //rest because on left side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+// const [pizza, , risotto, ...otherfoods] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherfoods);
+// // objects
 
-// 2) functions
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-};
-add(2, 3);
-add(5, 3, 7, 2);
-add(5, 3, 7, 2, 1, 2);
-const x = [23, 5, 7];
-add(...x);
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushroom');
+// // 2) functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(5, 3, 7, 2, 1, 2);
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushroom');
 
 // const arr = [7, 8, 9];
 // const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
