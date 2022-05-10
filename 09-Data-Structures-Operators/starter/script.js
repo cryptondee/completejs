f2b6d09262cb63ac12dd3a1e9614cdaabda397be
cryptondee <cryptondee@gmail.com>
2022-05-10 11:24:57 +0200

    Finished Section 9 Chapter 125

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -572,10 +572,6 @@ GOOD LUCK 😀
 // const [i, , [j, k]] = nested;
 // console.log(i, j, k);
 
-// // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // // Data needed for first part of the section
 // const restaurant = {
 //   name: 'Classico Italiano',
@@ -737,3 +733,21 @@ const planeInline = function (n) {
 };
 
 planeInline(5);
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flightArr = flights.split('+');
+console.log(flightArr);
+for (const item of flightArr) {
+  const [type, from, to, time] = item.split(';');
+  const output = `${type.startsWith('_Delayed') ? 'RED' : ' '}${type
+    .replaceAll('_', ' ')
+    .trim()
+    .padStart(20)} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`;
+
+  console.log(output);
+}
